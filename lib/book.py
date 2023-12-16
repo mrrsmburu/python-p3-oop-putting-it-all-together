959816662990a15a4b90ac58ce0a3a2d3e7917b0
mrrsmburu <mrrsmburu@gmail.com>
2023-12-16 16:12:53 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+     def __init__(self, title, page_count):
+        self.title = title
+        if not isinstance(page_count, int):
+            print("Error: page_count must be an integer.")
+        else:
+            self.page_count = page_count
+
+     def turn_page(self):
+        if self.current_page < self.page_count:
+            print("Flipping the page...wow, you read fast!")
+            self.current_page += 1
+        else:
+            print("You've reached the end of the book!")
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    def __init__(self, brand, size):
+        self.brand = brand
+        if not isinstance(size, int):
+            print("Error: size must be an integer.")
+        else:
+            self.size = size
+            self.condition = 'New'    
+   
     pass
